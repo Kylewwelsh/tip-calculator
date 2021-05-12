d3fbab872e3b4c077466b1387ad06d97d877fa76
kylewwelsh <kyle@kdmdental.com>
2021-05-12 16:23:23 -0600

    cheap tip alert message added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,13 @@ button.addEventListener(
   "click", ()=> {
     const billAmount = document.getElementById("bill-amount").value;
     const tipPercent = document.getElementById("tip-percent").value;
+      if (tipPercent <= 15) {
+        alert("That's a pretty cheap tip.. Lets try and bump that up a bit big spender..");
+        return;
+      }
     const billTimesTip = billAmount * tipPercent / 100;
     const total = billTimesTip.toFixed(2)
 
     document.getElementById('tip-total').innerHTML= total;
-
   }
 );
